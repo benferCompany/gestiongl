17073f8dad8b0314f6f7f1700e87ea2b2f8d182f
lautaro <lautyflowers07@gmail.com>
2025-08-23 17:53:27 -0300

    lauty

---
+++ vistas/body/body.js
@@ -0,0 +1,5 @@
+import { nav } from "./nav/nav.js";
+
+const body = document.body;
+
+body.appendChild(nav());
--- vistas/body/nav/nav.js
+++ vistas/body/nav/nav.js
@@ -1,9 +1,34 @@
-export const nav = () => {
-    return ` <nav>
-        <div>
-            <button>Inicio</button>
-            <button>Crear</button>
-            <button>Editar</button>
-        </div>
-    </nav>`
+const style = `
+    <style>
+        *  {
+            padding: 0;
+            margin: 0;
+            }
+        nav {
+            padding: 1rem;
+            display: flex;
+            justify-content: space-around;
+            background-color: #e3eeffff;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.48);
+        }
+        button {
+            background-color: #4c5cf0ff;
+            color: whitesmoke;
+            cursor: pointer;
+            border: none;
+            padding: 10px;
+            border-radius: 5px;
+        }
+    </style>`
+
+export const nav = function () {
+    const nav = document.createElement("nav");
+    nav.innerHTML = `
+        ${style}
+        <button>Ventas</button>
+        <button>Compras</button>
+        <button>Producto</button>
+        <button>Balances</button>
+    `;
+    return nav;
 }
