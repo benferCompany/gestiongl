45de3c8bdaeecc21224396f22d0cdb48d1e0438c
=Benjamin <bejidfer@gmail.com>
2025-08-30 10:55:08 -0300

    sdf

--- controladores/url/url.js
+++ controladores/url/url.js
@@ -1,4 +1,4 @@
-export const URL = "https://f267d7491a6eae80b7f141582dc2ec2c.serveo.net";
+export const URL = "https://40656b744c845d1c03606ce0c03a325d.serveo.net";
 
 /* tenes que descomentar y comentar el de arriba si queres trabajar en local
 export const URL = "localhost/gestionbl";
