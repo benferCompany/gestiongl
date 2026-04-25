d13b3d6246e747213e1873bb6feed7fa9690eb9b
benferCompany <benjifer@gmail.com>
2026-04-25 17:35:58 -0300

    arreglos en conexion

--- servicios/conexion.php
+++ servicios/conexion.php
@@ -2,7 +2,15 @@
 // ===================
 // CORS
 // ===================
-header("Access-Control-Allow-Origin: *");
+$allowed_origins = [
+    'https://gestionimportexcel.todoelectro.store',
+    'https://todoelectro.store'
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
+
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
